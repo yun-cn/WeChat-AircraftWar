16e8fd29c6693627cea6bb4a56988ec934516b2d
yunling zhang <yun313350095@gmail.com>
2018-04-08 23:34:49 +0900

    Create EnemyPlane

--- src/main.py
+++ src/main.py
@@ -29,6 +29,32 @@ class HeroPlane(object):
         self.bullet_list.append(Bullet(self.screen, self.x,  self.y))
 
 
+class EnemyPlane(object):
+    def __init__(self, screen_temp):
+        self.x  = 0
+        self.y  = 0
+        self.screen      = screen_temp
+        self.image       = pygame.image.load('./src/../image/enemy-1.png')
+        self.direction   = "right"
+
+    def display(self):
+        self.screen.blit(self.image, (self.x, self.y))
+
+    def move(self):
+        if self.x > 280:
+            self.direction = 'left'
+        elif self.x < 0:
+            self.direction = 'right'
+
+        if self.direction     == "right":
+            self.x += 5
+        elif self.direction   == "left":
+            self.x -= 5
+
+    def fire(self):
+        self.bullet_list.append(Bullet(self.screen, self.x, self.y))
+
+
 class Bullet(object):
     def __init__(self, screen_temp, x, y):
         self.x = x + 40
@@ -62,13 +88,16 @@ def key_control(hero_temp):
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((320, 568), 0, 32)
-    background = pygame.image.load('./src/../image/background.png')
-    hero = HeroPlane(screen)
+    screen      =   pygame.display.set_mode((320, 568), 0, 32)
+    background  =   pygame.image.load('./src/../image/background.png')
+    hero        =   HeroPlane(screen)
+    enemy       =   EnemyPlane(screen)
 
     while True:
         screen.blit(background, (0, 0))
         hero.display()
+        enemy.display()
+        enemy.move()
         pygame.display.update()
         key_control(hero)
         time.sleep(0.01)
